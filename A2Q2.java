52b43a8bed6c06233558b266bd52d91e639a465e
Biswajit Mahapatra <99376983+Biswajit-Mahapatra-02@users.noreply.github.com>
2022-04-18 14:56:45 +0530

    Create A2Q2.java

---
+++ A2Q2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Q2 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count=0;
+		do {
+			count++;
+		}while(n/2==0);
+		System.out.println("The number of times one should divide "+n+" by 2 until a number less than 2 is obtained is= "+count);
+		sc.close();
+	}
+}
