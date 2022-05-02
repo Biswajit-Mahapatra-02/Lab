f71f77d2740e6865fae012f97869f7bb36e20741
Biswajit Mahapatra <99376983+Biswajit-Mahapatra-02@users.noreply.github.com>
2022-05-02 14:38:56 +0530

    Update StudentDetails.java
    Added required method calls

--- Assignment1/StudentDetails.java
+++ Assignment1/StudentDetails.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 public class StudentDetails{
  public static void main(String args[]){
-   
+   Student A = new Student();
+   Student B = new Student();
+   Student C = new Student();
+   System.out.println("Enter details student 1: ");
+   A.setDetails();
+   System.out.println("Enter details student 2: ");
+   B.setDetails();
+   System.out.println("Enter details student 3: ");
+   C.setDetails();
+   System.out.println("Student 1 details: ");
+   A.display();
+   System.out.println("Student 2 details: ");
+   B.display();
+   System.out.println("Student 3 details: ");
+   C.display();
  }
 }
