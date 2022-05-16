4af8f6d828858fe26bacfafc1cd7ce0fad7a9d28
Biswajit Mahapatra <99376983+Biswajit-Mahapatra-02@users.noreply.github.com>
2022-05-16 16:01:41 +0530

    Q8 started but not solved

---
+++ Assignment2/Q8_Company/EmpInterface.java
@@ -0,0 +1,6 @@
+package Q8_Company;
+
+public interface EmpInterface {
+	void displayEmp();
+	void giveBonus(double amount);
+}
---
+++ Assignment2/Q8_Company/Manager.java
@@ -0,0 +1,19 @@
+package Q8_Company;
+
+public class Manager extends Employee{
+
+	public Manager(long empID, String fname, String lname, double salary) {
+		super(empID, fname, lname, salary);
+		
+	}
+
+	public void displayEmp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void giveBonus(double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+}
