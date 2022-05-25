5dc18d7dca673d95ba1896dbe2f7a54b9c833c20
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-05-25 14:12:21 +0530

    Assignment 1 complete

--- Assignment1/A1Q4.java
+++ Assignment1/A1Q4.java
@@ -6,24 +6,18 @@ public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     System.out.println("how many lines do you want to enter");
     int n = s.nextInt();
-
-    System.out.println("I want to enter " + n + " lines ");
-    n = n + 1;
-    String[] str;
-    str = new String[n];
-    int count = 0;
+    s.nextLine();
+    String[] str = new String[n];
+    System.out.println("Enter "+n+" lines => ");
     for (int i = 0; i < n; i++) {
         str[i] = s.nextLine();
-        System.out.println(str[i]);
-        count++;
     }
-    if (count == n) {
         System.out.println("Reversed output");
 
         for (int i = n - 1; i >= 0; i--) {
             System.out.println(str[i]);
             }
-        }
+        
         s.close();
     }
 }
