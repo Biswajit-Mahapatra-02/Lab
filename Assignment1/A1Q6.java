3e5351ff6a2dcc04a803dc0e3cd22982d3d3fb4d
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-05-25 20:27:04 +0530

    Assigment 1 complete

--- Assignment1/A1Q6.java
+++ Assignment1/A1Q6.java
@@ -3,17 +3,29 @@ public class A1Q6 {
 	public static void main(String args[]) {
 		int arr[]= {5,2,1,4,3};
 		System.out.println(withoutMethodMax(arr));
+		System.out.println(withoutMethodMin(arr));
 		System.out.println(withMethodMax(arr));
+		System.out.println(withMethodMin(arr));
 	}
 	
 	static int withoutMethodMax(int arr[]) {
-		 int max = 0;
+		 int max = arr[0];
 		 for(int i = 0;i<arr.length;i++) 
 			 if(arr[i]>max)
 				 max = arr[i];
 		 return max;
 	}
+	static int withoutMethodMin(int arr[]) {
+		int min = arr[0];
+		for(int i = 1;i<arr.length;i++) 
+			if(arr[i]<min)
+				min = arr[i];
+		return min;
+   }
 	static int withMethodMax(int arr[]) {
 		return Arrays.stream(arr).max().getAsInt();
+	}
+	static int withMethodMin(int arr[]) {
+		return Arrays.stream(arr).min().getAsInt();
 	}
 }
