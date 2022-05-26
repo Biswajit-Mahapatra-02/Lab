338ff1f988d9c93dd876f768e208bd612bfa6eb9
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-05-26 17:23:41 +0530

    Assignment 1 complete

--- Assignment1/A1Q7.java
+++ Assignment1/A1Q7.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
 public class A1Q7{
-  
-  
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("How many numbers are to be taken as input?: ");
     int n = sc.nextInt();
     int[] arr = new int[n];
-    System.out.println("Enter "+ n + " numbers => ");
+    System.out.print("Enter "+ n + " numbers => ");
     for (int i = 0; i < arr.length; i++)
       arr[i]=sc.nextInt();
-    for (int i = 0; i < arr.length; i++) {
-      // int currentNumber = arr[i];
-      for (int j = 0; j < arr.length; j++) {
+    System.out.println("Pairs of distinct elements of the array whose product is odd => ");
+    for (int i = 0; i < arr.length; i++) 
+      for (int j = 0; j < arr.length; j++) 
         if(arr[i]!=arr[j])
           if((arr[i]*arr[j])%2!=0)
-            System.out.println("a pair of distinct elements of the array whose product is odd: ("+arr[i]+","+arr[j]+")");
-      }
-    }
+            System.out.print("("+arr[i]+","+arr[j]+")  ");
       sc.close();
   }
 }
