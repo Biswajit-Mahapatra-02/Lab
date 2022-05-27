dc6ef56fd75c96e643ef74df14c866dffa5ca4de
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-05-27 18:26:17 +0530

    Assignment 1 complete

--- Assignment1/StudentQ10.java
+++ Assignment1/StudentQ10.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
 public class StudentQ10{
   private String name;
   private int roll;
   private double mark;
   void setData(String name, int roll, double mark ){
-    Scanner sc = new Scanner (System.in);
     this.name = name;
     this.roll = roll;
     this.mark = mark;
-    sc.close();
   }
   void display(){
       System.out.println("Name: "+name);
