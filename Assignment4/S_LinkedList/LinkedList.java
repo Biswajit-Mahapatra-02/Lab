c0f65fa9990efafed9d4a24436c3a764be7ec336
Biswajit Mahapatra <99376983+Biswajit-Mahapatra-02@users.noreply.github.com>
2022-06-20 15:11:27 +0530

    Update LinkedList.java

--- Assignment4/S_LinkedList/LinkedList.java
+++ Assignment4/S_LinkedList/LinkedList.java
@@ -1,5 +1,32 @@
 package S_LinkedList;
 
+import java.util.Scanner;
 public class LinkedList{
   Node start; 
+  void create(){
+    node A=new node();
+    do{
+      Node P = new Node();
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Input node value: ");
+      P.info = sc.nextInt();
+      P.link = null;
+      if(start==null){
+        start = P;
+        A = P;
+      } else {
+        A.next = P;
+        A = P;
+      }
+      System.out.println("Do you want to add more nodes?(y/N)");
+      char c = sc.next().charAt(0);
+    }while(c=='Y');
+  }
+  void display(){
+    Node P = start;
+    while(P.link!=null){
+       System.out.println("Node information: "+P.info);
+       P=P.link;
+    }
+  }
 }
